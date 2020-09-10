8f8dadab4e02ef9734bb59ec8130f5b0b23bc18d
Adrian Montenegro <isc.montenegro@gmail.com>
2020-09-09 19:25:17 -0500

    Changes to the scrapper script

--- scapper.py
+++ scapper.py
@@ -1,14 +1,13 @@
-import urllib2
+import requests
 from bs4 import BeautifulSoup
 
 fifa_page = 'https://www.fifa.com/worldcup/groups/'
-req = urllib2.Request(fifa_page, headers={'User-Agent' : "Magic Browser"})
-fifa_soup = urllib2.urlopen(req)
-soup = BeautifulSoup(fifa_soup, 'html.parser')
+resp = requests.get(fifa_page)
+soup = BeautifulSoup(resp, 'html.parser')
 
 
 name_box = soup.find('h1', attrs={'class': 'fi-pageheader__title'})
 
 name = name_box.text.strip()
 
-print name
+print(name)
